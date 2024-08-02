0ffbd64370d499fe2322f9b75d78c34ada953cd9
Akshay Kamble <153273927+akshaykamble786@users.noreply.github.com>
2024-08-02 08:26:45 +0000

    configured the environment

---
+++ bloggit/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  return (
+    <>
+      <h2>This is a blog app</h2>
+    </> 
+  )
+}
+
+export default App
